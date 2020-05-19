569bb703656f845cd137c9fae9ccd98d899d8c18
孙雨亮 <sunyuliang@beisen.com>
2020-05-19 19:15:02 +0800

    F:添加异常处理 抛出异常的例子

---
+++ StudyException/ThrowAndThrowEx.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudyException
+{
+    public static class ThrowAndThrowEx
+    {
+        public static void ThrowEx()
+        {
+            throw new Exception("test throw ex");
+        }
+
+        public static void Demo1()
+        {
+            try
+            {
+                ThrowEx();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        //如果是需要抛出的类型，需要直接throw 不然会丢失堆栈信息
+        public static void Demo2()
+        {
+            try
+            {
+                ThrowEx();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
