977a278171bbf876ef573e5b6d0b2a131c6f2f50
孙雨亮 <sunyuliang@beisen.com>
2020-07-07 17:38:28 +0800

    O:临时

---
+++ TestError1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TestError1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("开始");
+            Thread.Sleep(1000);
+            var a = 0;
+            var b = 239 / a;
+            List<int> testList = new List<int>();
+            ConcurrentBag<Task> tList = new ConcurrentBag<Task>();
+            for (int i = 0; i < 10000; i++)
+            {
+                var t = Task.Run(() =>
+                  {
+                      testList.Add(i);
+                      testList.Add(i);
+                      testList.Add(i);
+                  });
+                tList.Add(t);
+            }
+            Task.WaitAll(tList.ToArray());
+            Console.WriteLine("完成");
+            Console.ReadKey();
+        }
+    }
+}
