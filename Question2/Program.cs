3c4b4bf140efc7312e2e2bd651913de47ca16b50
孙雨亮 <sunyuliang@beisen.com>
2020-11-17 15:46:55 +0800

    O:添加ThreadContext的学习

---
+++ Question2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //这里有点问题，跑了很久没抛异常
+            List<int> list = new List<int>();
+            List<Task> taskList = new List<Task>();
+            for (int i = 0; i < 80; i++)
+            {
+              var t=  Task.Run(()=> {
+                    for (int j = 0; j < 100; j++)
+                    {
+                        list.Add(j);
+                    }
+                  throw new Exception();
+                });
+                taskList.Add(t);
+            }
+            Task.WaitAll(taskList.ToArray());
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("结束");
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ ThreadContextStudy/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ThreadContextStudy
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            CallContext.LogicalSetData("key1", "value1");
+            CallContext.SetData("key2", "value2");
+
+            Task.Run(() =>
+            {
+                Task.Run(() =>
+                {
+                    var value1 = CallContext.LogicalGetData("key1");
+                    var value2 = CallContext.GetData("key2");
+                    Console.WriteLine($"value1:{value1},value2:{value2}");
+                });
+            });
+            Console.ReadKey();
+        }
+    }
+}
