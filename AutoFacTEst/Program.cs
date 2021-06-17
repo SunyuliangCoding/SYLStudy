1404e5541eeaf6dfe6067ca45b3e6087b34cee5a
孙雨亮 <sunyuliang@beisen.com>
2021-06-17 16:40:18 +0800

    临时

---
+++ AutoFacTEst/Program.cs
@@ -0,0 +1,41 @@
+﻿using Autofac;
+using AutoFaceService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoFacTEst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //AutoFaceService.dll
+            var ass = Assembly.LoadFile(System.Environment.CurrentDirectory+"/AutoFaceService.dll");
+
+            var builder1 = new ContainerBuilder();
+            builder1.RegisterAssemblyTypes(ass)
+    .Where(t => t.Name.EndsWith("Service"))
+    .AsSelf().AsImplementedInterfaces().SingleInstance()
+    .PropertiesAutowired(PropertyWiringOptions.AllowCircularDependencies);
+            builder1.Register(n => new AService())
+              .As<AService>().SingleInstance();
+
+
+            builder1.Build();
+
+            builder1.Register(n => new AService())
+         .As<AService>().SingleInstance();
+            builder1.RegisterAssemblyTypes(ass)
+ .Where(t => t.Name.EndsWith("Service"))
+ .AsSelf().AsImplementedInterfaces().SingleInstance()
+ .PropertiesAutowired(PropertyWiringOptions.AllowCircularDependencies);
+
+            builder1.Build();
+        }
+    }
+
+}
---
+++ MaxMmemory/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaxMmemory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> list = new List<string>();
+
+            for (int i = 0; i < int.MaxValue; i++)
+            {
+                list.Add(string.Join(",", Enumerable.Range(0, 10000)));
+            }
+
+            Console.ReadLine();
+        }
+
+    }
+}
